afe0fb15c2fc5253d41249b9f821b791753f377a
Alexlaaa <leealessandro.sg@gmail.com>
2024-08-23 21:16:44 +0800

    feat: UI for bluetooth page

--- app/src/main/java/com/example/mdp_android/BluetoothPage.java
+++ app/src/main/java/com/example/mdp_android/BluetoothPage.java
@@ -1,9 +1,6 @@
 package com.example.mdp_android;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class BluetoothPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bluetooth_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // TODO: Remove/refactor later, currently for testing bluetooth page
-        Button bluetoothButton = findViewById(R.id.button);
-        bluetoothButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to start BluetoothPage activity
-                Intent intent = new Intent(MainActivity.this, BluetoothPage.class);
-                startActivity(intent);
-            }
         });
     }
 }
